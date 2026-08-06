c1783112820428f430f0d43dbf30a0656e20c49c
Albert Manoj <albert.manoj@anblicks.com>
2026-08-06 16:39:37 +0530

    env bug fix

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,6 @@
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config();
 
 export const config = {
   port: Number(process.env.PORT || 3000),
